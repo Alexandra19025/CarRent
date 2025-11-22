33cc44f82a686b9be272b6e37d445da460d41d14
alexa <alexandrasakellariade@gmail.com>
2025-11-22 12:38:51 +0200

    ll

---
+++ src/AllCars.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class AllCars {
+    ArrayList<Car> allCars;
+    public AllCars() {
+        allCars = new ArrayList<>();
+    }
+    public boolean addCar(Car car) {
+        for(Car c : allCars) {
+            if(c.equals(car)) {
+                return false;
+            }
+        }
+        allCars.add(car);
+        return true;
+    }
+    public Car getCar(String id) {
+        for(Car c : allCars) {
+            if(c.getId().equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Car> getCar(String carBrand, String carModel, String carColor, String carSituation) {
+        ArrayList<Car> carList = new ArrayList<>();
+
+        for(Car c : allCars) {
+            boolean flag = true;
+            if(carBrand!=null){
+                if(!(c.getCarBrand().equals(carBrand))){
+                    flag = false;
+                }
+            }
+            if(carModel!=null){
+                if(!c.getModel().equals(carModel)) {
+                    flag = false;
+                }
+            }
+            if(carColor!=null){
+                if (!c.getColor().equals(carColor)) {
+                    flag = false;
+                }
+            }
+            if(carSituation!=null){
+                if(!c.getSituation().equals(carSituation)) {
+                    flag = false;
+                }
+
+            }
+            if(flag){
+                carList.add(c);
+            }
+        }
+        return carList;
+    }
+
+
+}
---
+++ src/AllClients.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class AllClients {
+    ArrayList<Client> clients;
+    public AllClients() {
+        clients = new ArrayList<>();
+    }
+    public boolean addClient(Client client) {
+        for (Client c : clients) {
+            if(client.equals(c)) {
+                return false;
+            }
+        }
+        clients.add(client);
+        return true;
+    }
+    public Client searchClientByAFM(String AFM) {
+        for (Client c : clients) {
+            if(c.getAFM().equals(AFM)) {
+                return c;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Client> searchByName(String firstName, String lastName) {
+        ArrayList<Client> clientsByName = new ArrayList<>();
+        for (Client c : clients) {
+            if(firstName.equals(c.getFirst_name()) && lastName.equals(c.getLast_name())) {
+                clientsByName.add(c);
+            }
+        }
+        return clientsByName;
+    }
+    public Client searchClientByPhone(String phone) {
+        for (Client c : clients) {
+            if(c.getPhone().equals(phone)) {
+                return c;
+            }
+        }
+        return null;
+    }
+}
